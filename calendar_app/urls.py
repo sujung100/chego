e8341d652ae5544cd30c193d5259f71bab5315be
sujung100 <myrecord100@gmail.com>
2023-08-03 11:23:15 +0900

    230803 / bsj / 수정전 백업

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     # 변경
 
-  path('', views.First_list.as_view(), name='First_list'),
+  path('', views.Idx_list.as_view(), name='Idx_list'),
+  path('first/', views.First_list.as_view(), name='First_list'),
   path('reserve/', views.reserve, name='reserve'),
   # path('info/', views.info, name='info'),
   path('write/', views.write, name='write'),
-  path('test/', views.Idx_list.as_view(), name='Idx_list'),
+  path('test/', views.Test_list.as_view(), name='Test_list'),
 ]

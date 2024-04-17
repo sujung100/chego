126935dd8fb73493fc6ac7b9ada370c59f733fde
sujung100 <myrecord100@gmail.com>
2024-04-17 20:18:15 +0900

    배포테스트-17 redis

--- chego_pjt/settings.py
+++ chego_pjt/settings.py
@@ -176,10 +176,10 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             # "hosts": [("127.0.0.1", 6379)],
-            # "hosts": [("svc.sel5.cloudtype.app", 6379)],
-            "hosts": [("svc.sel5.cloudtype.app", 30001)],
+            "hosts": [("svc.sel5.cloudtype.app:30001", 6379)],
+            # "hosts": [("svc.sel5.cloudtype.app", 30001)],
         },
-        "MIDDLEWEAR" : [
+        "MIDDLEWARE" : [
             "channels.middleware.AuthMiddlewareStack"
         ],
     },

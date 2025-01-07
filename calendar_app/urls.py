dd52d1fc0039c5c0d92271162a83273c4cdbb415
sujung100 <myrecord100@gmail.com>
2025-01-07 21:19:55 +0900

    calendar_app 미사용파일 정리

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -3,43 +3,14 @@ from . import views
 # from doit_django.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
-    # 변경
-
   path('', views.Idx_list.as_view(), name='Idx_list'),
-  # path('reservation/', views.FindReservationView.as_view(), name='Reserve_list'),
-
-  # path('reservation/', views.FindReservationView.as_view(), name='Reserve_list'),
-  # path('reservation_detail/<int:reservation_id>/', views.DetailView.as_view(), name='Detail_view'),
-
-
   path('find_reservation/', views.FindReservationView.as_view(), name='find_reservation'),
   path('input_user_pw/', views.InputUserNameView.as_view(), name='input_user_pw'),
-  # path('detail_view/', views.DetailView.as_view(), name='detail_view'),
-  # path('delete_fin/', views.ReservationDeletedView.as_view(), name='reservation_deleted_view'),
 
-
-
-  # 비동기 url 생성
-  # path('detail_view/pass/', views.FetchView.as_view(), name='fetch_view'),
+  # 비동기 url
   path('input_user_pw/pass/', views.FetchView.as_view(), name='fetch_view'),
-  # path('pre_choice/', views.FetchCheckRsv.as_view(), name='pre_choice'),
-
-  # 24.12.22수정
-  path('send_msg/', views.Idx_list.as_view(), name='send_msg'),
-
-  # path('send_msg/', views.FetchTest.as_view(), name='send_msg'),
-
-
-
-  # path('first/', views.First_list.as_view(), name='First_list'),
-  # path('reserve/', views.reserve, name='reserve'),
-  # path('info/', views.info, name='info'),
-  # path('write/', views.write, name='write'),
-  
-  # 성우님꺼 테스트(주석처리)
-  path('test/', views.Test_list.as_view(), name='Test_list'),
-  # Idx_list ajax
-   path("api/store_calendar/<int:store_id>/", views.Store_calendar.as_view(), name="store_calendar"),
+  # path('send_msg/', views.Idx_list.as_view(), name='send_msg'),
+  path("api/store_calendar/<int:store_id>/", views.Store_calendar.as_view(), name="store_calendar"),
 
 
   
